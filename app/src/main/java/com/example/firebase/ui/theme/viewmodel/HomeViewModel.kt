dfcfebbf0c1d5af39ef14b848b0d69bc078e95a8
iqwa1107 <127074183+iqwa1107@users.noreply.github.com>
2025-01-12 02:57:34 +0700

    Menambahkan fun deleteMhs

--- app/src/main/java/com/example/firebase/ui/theme/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/firebase/ui/theme/viewmodel/HomeViewModel.kt
@@ -38,6 +38,16 @@ class HomeViewModel (
                 }
         }
     }
+    fun deleteMhs(mahasiswa: Mahasiswa){
+        viewModelScope.launch {
+            try {
+                mhs.deleteMhs(mahasiswa)
+            } catch (e: Exception) {
+                mhsUiState = HomeUiState.Error(e)
+            }
+
+        }
+    }
 }
 
 
