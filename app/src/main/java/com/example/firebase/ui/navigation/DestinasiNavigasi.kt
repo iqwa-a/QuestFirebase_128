92cb3b2765a3d5e6fbb03b9fb6b69899c7ed2009
iqwa1107 <127074183+iqwa1107@users.noreply.github.com>
2025-01-12 12:39:49 +0700

    Membuat DestinasiNavigasi

---
+++ app/src/main/java/com/example/firebase/ui/navigation/DestinasiNavigasi.kt
@@ -0,0 +1,14 @@
+package com.example.firebase.ui.navigation
+
+interface DestinasiNavigasi {
+    val route: String
+    val titleRes: String
+}
+object DestinasiHome : DestinasiNavigasi {
+    override val route: String = "home"
+    override val titleRes: String = "Home"
+}
+object DestinasiInsert : DestinasiNavigasi {
+    override val route: String = "insert"
+    override val titleRes: String = "Insert"
+}
