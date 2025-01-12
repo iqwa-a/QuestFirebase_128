1a39d226d0a097f4a93c675379f47eb497163adb
iqwa1107 <127074183+iqwa1107@users.noreply.github.com>
2025-01-12 14:19:11 +0700

    Memperbarui State berdasarkan input

--- app/src/main/java/com/example/firebase/ui/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/firebase/ui/viewmodel/InsertViewModel.kt
@@ -60,4 +60,23 @@ class InsertViewModel (
 
     var uiState: FormState by mutableStateOf(FormState.Idle)
         private set
+    // Memperbarui State berdasarkan input
+    fun updateUiEvent(event: MahasiswaEvent) {
+        uiEvent = uiEvent.copy(insertUiEvent = event)
+    }
+
+    // Validasi data input pengguna
+    fun validateFields (): Boolean {
+        val event = uiEvent.insertUiEvent
+        val errorState = FormErrorState(
+            nim = if (event.nim.isEmpty()) "NIM tidak boleh kosong" else null,
+            nama = if (event.nama.isEmpty()) "Nama tidak boleh kosong" else null,
+            gender = if (event.gender.isEmpty()) "Jenis Kelamin tidak boleh kosong" else null,
+            alamat = if (event.alamat.isEmpty()) "Alamat tidak boleh kosong" else null,
+            kelas = if (event.kelas.isEmpty()) "Kelas tidak boleh kosong" else null,
+            angkatan = if (event.angkatan.isEmpty()) "Angkatan tidak boleh kosong" else null
+        )
+        uiEvent = uiEvent.copy(isEntryValid = errorState)
+        return errorState.isValid()
+    }
 }
