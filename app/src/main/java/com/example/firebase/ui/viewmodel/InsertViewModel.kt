62d916812382d6125c6c85b2a3ea26c361941fd2
iqwa1107 <127074183+iqwa1107@users.noreply.github.com>
2025-01-12 13:45:40 +0700

    Menambahkan MahasiswaEvent, fungsi konversi ke Mahasiswa, dan FormErrorState dengan validasi

---
+++ app/src/main/java/com/example/firebase/ui/viewmodel/InsertViewModel.kt
@@ -0,0 +1,41 @@
+package com.example.firebase.ui.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.firebase.model.Mahasiswa
+import com.example.firebase.repository.RepositoryMhs
+import kotlinx.coroutines.launch
+
+data class MahasiswaEvent (
+    val nim: String = "",
+    val nama: String = "",
+    val gender: String = "",
+    val alamat: String = "",
+    val kelas: String = "",
+    val angkatan: String = ""
+)
+
+fun MahasiswaEvent.toMahasiswa(): Mahasiswa = Mahasiswa(
+    nim = nim,
+    nama = nama,
+    gender = gender,
+    alamat = alamat,
+    kelas = kelas,
+    angkatan = angkatan
+)
+
+data class FormErrorState (
+    val nim: String? = null,
+    val nama: String? = null,
+    val gender: String? = null,
+    val alamat: String? = null,
+    val kelas: String? = null,
+    val angkatan: String? = null
+) {
+    fun isValid(): Boolean {
+        return nim == null && nama == null && gender == null && alamat == null && kelas == null && angkatan == null
+    }
+}
