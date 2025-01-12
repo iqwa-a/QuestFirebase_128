c01e7a0069a3567651e36336f37e4a4e23117014
iqwa1107 <127074183+iqwa1107@users.noreply.github.com>
2025-01-12 20:38:15 +0700

    Memperbaiki PenyediaViewModel

---
+++ app/src/main/java/com/example/firebase/ui/viewmodel/PenyediaViewModel.kt
@@ -0,0 +1,17 @@
+package com.example.firebase.ui.viewmodel
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.firebase.MahasiswaApp
+
+fun CreationExtras.aplikasiMahasiswa(): MahasiswaApp =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as MahasiswaApp)
+
+object PenyediaViewModel{
+    val Factory = viewModelFactory {
+        initializer { HomeViewModel(aplikasiMahasiswa().containerApp.repositoryMhs)}
+        initializer { InsertViewModel(aplikasiMahasiswa().containerApp.repositoryMhs)}
+    }
+}
